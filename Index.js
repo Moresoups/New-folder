b3d8751f31525793e88903608407e9680de177ad
Moresop <gardner_christopher@wheatoncollege.edu>
2022-12-09 18:19:46 -0500

    promt user is broken but its taking shape

--- Index.js
+++ Index.js
@@ -6,104 +6,112 @@ const Other = require("./lib/other");
 
 const inquirer = require("inquirer");
 const fs = require("fs");
-inquirer.registerPrompt("loop", require("inquirer-loop")(inquirer));
+//inquirer.registerPrompt("loop", require("inquirer-loop")(inquirer));
 
 
 const generateHtml = require("./utils/generateHtml");
 //seperate questions for each 
 //remove loop seperate, 4 sets of questions
-const questions = () => {
+const questionManager = () => {
     return inquirer.prompt([
         {
-            type: "checkbox",
-            message: "What is the role of this Team member",
-            name: "teamMember",
-            choices: [
-                "Manager",
-                "Engineer",
-                "Intern",
-                "other"
-            ],
-        },
-        {
-            when: input => {
-                return input.teamMember === "Manager"
-            },
             type: "input",
             message: "Please enter the team manager's name and ID number.",
             name: "Mgmt",
             default: "No name listed"
         },
         {
-            if: input => {
-                return input.teamMember === "Manager"
-            },
             type: "input",
             message: "Please enter the manager's email address.",
             name: "Email",
             default: "No Email given"
         },
         {
-            if: input => {
-                return input.teamMember === "Manager"
-            },
             type: "input",
             message: "Please enter the manager's office number.",
             name: "ONumber",
             default: "No office number given"
         },
         {
-            if: input => {
-                return input.role === "Engineer" || "Intern" || "other"
-            },
             type: "input",
             message: "Please enter the employee's name.",
             name: "Ename",
             default: "no name given"
         },
+    ])
+}
+const QuestionOther = () => {
+
+    return inquirer.prompt([
         {
-            if: input => {
-                return input.role === "Engineer" || "Intern" || "other"
-            },
             type: "input",
-            message: "Please enter the employee's ID.",
+            message: "Please enter the employee's name and ID.",
             name: "id",
             default: "no ID given"
         },
         {
-            if: input => {
-                return input.role === "Engineer" || "Intern" || "other"
-            },
             type: "input",
             message: "Please enter the employee's email address.",
             name: "email",
             default: "no email given"
         },
         {
-            if: input => {
-                return input.role === "Engineer"
-            },
             type: "input",
             message: "Please enter the employee's GitHub username.",
             name: "gitHub",
             default: "no github given"
         },
         {
-            if: input => {
-                return input.role === "Intern"
-            },
             type: "input",
             message: "Please enter the intern's school.",
             name: "school",
             default: "no school given"
         },
         {
-            type: "loop",
-            message: "Would you like to add a team member? type (n) to generate HTML, select (y) to generate another team member",
-            name: "addEmployee",
-        },
-    ]);
+            type: 'confirm',
+            name: 'NewMember',
+            message: 'Add another team member?',
+            default: false
+        }
+    ])
+
+        .then(managerInput => {
+            const { Mgmt, Email, ONumber, Ename } = managerInput;
+            const manager = new Manager(Mgmt, Email, ONumber, Ename);
+
+            teamArray.push(manager);
+            console.log(manager);
+        })
+        .then(employeeData => {
+            let { id, email, github, school, NewMember } = employeeData;
+            let employee;
+            if (role === "Engineer") {
+                employee = new Engineer(id, email, github);
+
+                console.log(employee);
+
+            } else if (role === "Intern") {
+                employee = new Intern(id, email, school);
+
+                console.log(employee);
+            } if (NewMember) {
+                return addEmployee(teamArray);
+            } else {
+                return teamArray;
+            }
+        })
 }
+function promptUser() 
+  .then(addEmployee)
+        .then(teamArray => {
+            return generateHTML(teamArray);
+        })
+        .then(pageHTML => {
+            return writeToFile(pageHTML);
+        })
+        .catch(err => {
+            console.log(err);
+        });
 
 function writeToFile(fileName, data) {
     fs.writeFile(fileName, data, err => {
@@ -114,14 +122,4 @@ function writeToFile(fileName, data) {
         console.log("HTML file succesfully generated")
     });
 }
-
-function promptUser() {
-    questions()
-        .then((response) => {
-            const madeHtml = generateHtml(response);
-            writeToFile('index.html', madeHtml);
-        });
-}
-
-
 promptUser();
